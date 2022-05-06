7eeb67e082334892593bda95e36b0fa2513cf9ce
Yoz Grahame <ygrahame@launchdarkly.com>
2022-05-06 14:50:28 -0700

    Yoz-specific config

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { asyncWithLDProvider } from "launchdarkly-react-client-sdk";
 import { deviceType, osName } from "react-device-detect";
 import getUserId from "./util/getUserId";
 
-const CLIENTKEY = "609ead905193530d7c28647b";
+const CLIENTKEY = "62758f35b099aa154122f7ca";
 
 let id = getUserId();
 
